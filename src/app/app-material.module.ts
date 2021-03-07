9ba0b2091292d276c7583e00d3b59fc3dac86785
abolnoor <eng.fadi12@gmail.com>
2021-03-07 16:30:34 +0200

    initial commit

---
+++ src/app/app-material.module.ts
@@ -0,0 +1,38 @@
+import {NgModule} from '@angular/core';
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatSortModule} from '@angular/material/sort';
+import {MatTableModule} from '@angular/material/table';
+import {MatRippleModule} from '@angular/material/core';
+import {MatCardModule} from '@angular/material/card';
+
+
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [],
+  exports: [
+    MatButtonModule,
+    MatToolbarModule,
+    MatAutocompleteModule,
+    MatDividerModule,
+    MatIconModule,
+    MatTooltipModule,
+    MatProgressBarModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatTableModule,
+    MatRippleModule,
+    MatCardModule
+  ]
+})
+export class AppMaterialModule { }
